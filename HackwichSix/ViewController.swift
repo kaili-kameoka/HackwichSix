f69b79b62bdbe4e900ba53f51afd17afef6d392f
Kaili Kameoka <kaili.kameoka@gmail.com>
2022-03-02 11:41:10 -1000

    Completed HackwichSix

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var myFriends = ["Sara", "Nicole", "Grant"];
+    var cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier");
+        let text = cities[indexPath.row];
+        cell?.textLabel?.text = text;
+        return cell!;
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
