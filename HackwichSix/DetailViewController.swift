ce3b7fc4219f160b1bd879276289d557cd5a0a37
Kaili Kameoka <kaili.kameoka@gmail.com>
2022-03-22 21:06:17 -1000

    Completed Hackwich #10

---
+++ HackwichSix/DetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DetailViewController.swift
+//  HackwichSix
+//
+//  Created by Kaili Kameoka on 3/22/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imagePass: String?;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        if let imageName = imagePass {
+            imageView.image = UIImage(named: imageName);
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -8,25 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    var myFriends = ["Sara", "Nicole", "Grant"];
+    
+    var myFriends = ["Kalapawai"];
     var cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
+    var myFriendsHomesArray = ["Kapolei"];
+
+    var restaurantImageData = [String]();
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cities.count;
+        return myFriends.count;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier");
-        let text = cities[indexPath.row];
+        let text = myFriends[indexPath.row];
         cell?.textLabel?.text = text;
+        cell?.detailTextLabel?.text = myFriendsHomesArray[indexPath.row]
         return cell!;
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true);
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let path = Bundle.main.path(forResource: "PropertyList", ofType: "plist");
+        let dict = NSDictionary(contentsOfFile: path!);
+        restaurantImageData = dict!.object(forKey: "RestaurantImages") as! [String];
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "mySegue" {
+            let s1 = segue.destination as! DetailViewController;
+            let imageIndex = tableView.indexPathForSelectedRow?.row
+            s1.imagePass = restaurantImageData[imageIndex!];
+        }
+        
+        
+    }
     
 
 }
